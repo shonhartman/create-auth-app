899d8d91ad8840558e764d2f2ebec8f425b81386
shonhartman <shaun@bighuman.com>
2022-08-03 12:41:36 -0400

    working prototype

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+import Dashboard from "./Dashboard";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login/>} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
